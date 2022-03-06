23fcfb872a2e0120f9e24ce3f11cc9f5df9f263e
DESKTOP-RDSDOUL\Gon <krdn.net@gmail.com>
2022-03-06 17:49:40 +0900

    솔루션 폴더 배치 정리

--- src/DataAccess/PocketBook/Data/UnitOfWork.cs
+++ src/DataAccess/PocketBook/Data/UnitOfWork.cs
@@ -4,6 +4,11 @@ using PocketBook.Core.Repositories;
 
 namespace PocketBook.Data
 {
+    /// <summary>
+    /// https://docs.microsoft.com/ko-kr/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application#creating-the-unit-of-work-class
+    ///
+    /// https://enlear.academy/repository-pattern-and-unit-of-work-with-asp-net-core-web-api-6802e1aa4f78
+    /// </summary>
     public class UnitOfWork : IUnitOfWork, IDisposable
     {
         private readonly ApplicationDbContext _context;
