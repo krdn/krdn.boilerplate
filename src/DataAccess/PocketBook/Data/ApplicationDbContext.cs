e2e058b0fff86c5e6e56ae1a7ca21f148f7ae2f1
Gon <krdn.net@gmail.com>
2022-03-16 22:28:46 +0900

    주석

--- src/DataAccess/PocketBook/Data/ApplicationDbContext.cs
+++ src/DataAccess/PocketBook/Data/ApplicationDbContext.cs
@@ -5,6 +5,10 @@ namespace PocketBook.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        // virtual을 사용하는 이유: https://quick-adviser.com/what-is-the-use-of-virtual-property/
+        // Lazy loading, Test를 위해 사용한다고 되어 있는데 정확한 이유는 모름.
+        // 최신 셈플에서는 virtual 키워드를 사용하고 있지 않음.
+        // https://docs.microsoft.com/ko-kr/aspnet/core/data/ef-mvc/intro?view=aspnetcore-6.0#create-the-database-context
         public virtual DbSet<User> Users { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
@@ -18,3 +22,4 @@ namespace PocketBook.Data
         }
     }
 }
+ 
