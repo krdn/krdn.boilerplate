27f44349e94dd748ba7bcc3dcd4f653cfeb1572e
Gon <krdn.net@gmail.com>
2022-03-18 17:20:23 +0900

    UoW-101 파일 추가

---
+++ src/DataAccess/UoW-101/Data/DBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UoW_101.Model;
+
+namespace UoW_101.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
