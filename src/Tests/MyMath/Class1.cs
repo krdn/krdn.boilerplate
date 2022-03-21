8d0672ad5ddd081f08ad32b1e511c30efded7fde
Gon <krdn.net@gmail.com>
2022-03-21 11:42:24 +0900

    연습: 테스트 탐색기를 사용한 테스트 기반 개발

---
+++ src/Tests/MyMath/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace MyMath
+{
+    public class Rooter
+    {
+        public Rooter()
+        {
+        }
+
+        public double SquareRoot(double input)
+        {
+            if (input <= 0.0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            double result = input;
+            double previousResult = -input;
+            while (Math.Abs(previousResult - result) > result / 1000)
+            {
+                previousResult = result;
+                result = (result + input / result) / 2;
+                //result = result - (result * result - input) / (2 * result);
+            }
+            return result;
+        }
+    }
+}
